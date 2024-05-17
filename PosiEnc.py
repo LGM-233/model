d73b7ef5c5fc65affb34c80806d720fd1cc40420
LGM-233 <134917695+LGM-233@users.noreply.github.com>
2024-05-17 15:36:08 +0800

    Update PosiEnc.py

--- PosiEnc.py
+++ PosiEnc.py
@@ -3,14 +3,14 @@ class PositionalEncoding(torch.nn.Module):
     def __init__(self, max_num_seg, d_model, num_seg, seg_len):
         super(PositionalEncoding, self).__init__()
 
-        # 计算位置编码矩阵
+        
         if seg_len == 512:
             position = torch.arange(seg_len).unsqueeze(1)
             div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float32) * -(torch.log(torch.tensor(10000.0)) / d_model))
             pos_enc = torch.zeros((seg_len, d_model),dtype=torch.float32)
             pos_enc[:, 0::2] = torch.sin(position * div_term)
             pos_enc[:, 1::2] = torch.cos(position * div_term)
-            pos_enc = pos_enc.unsqueeze(0)  # 添加 batch 维度
+            pos_enc = pos_enc.unsqueeze(0)  
             self.register_buffer('pos_enc', pos_enc)
         elif seg_len < 512:
             position = torch.arange(0, max_num_seg, dtype=torch.float32).unsqueeze(1)
@@ -26,10 +26,10 @@ class PositionalEncoding(torch.nn.Module):
             pos_enc = torch.zeros((seg_len, d_model), dtype=torch.float32)
             pos_enc[:, 0::2] = torch.sin(position * div_term)
             pos_enc[:, 1::2] = torch.cos(position * div_term)
-            pos_enc = pos_enc.unsqueeze(0)  # 添加 batch 维度
+            pos_enc = pos_enc.unsqueeze(0) 
             self.register_buffer('pos_enc', pos_enc)
 
     def forward(self, x):
-        # 将位置编码矩阵添加到输入中
+        
         x = x + self.pos_enc
         return x
