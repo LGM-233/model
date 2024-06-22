938b184ca6a9387f05d0df79776cfe81507eeb5a
LGM-233 <134917695+LGM-233@users.noreply.github.com>
2024-06-22 20:28:54 +0800

    Update LSTM.py

--- compare experiment/LSTM.py
+++ compare experiment/LSTM.py
@@ -12,17 +12,17 @@ from torch.optim.lr_scheduler import StepLR
 from xlnet_hierarchical_attn.Bilstm import BiLSTM
 # from xlnet_hierarchical_attn.train1 import NyAttentioin
 from xlnet_hierarchical_attn.LSTMmodel import LSTM
-from 词嵌入训练.PosiEnc import PositionalEncoding
-from 词嵌入训练.Focal_loss import FocalLoss
-from 词嵌入训练.ASL_loss import AsymmetricLoss
+from PosiEnc import PositionalEncoding
+from Focal_loss import FocalLoss
+from ASL_loss import AsymmetricLoss
 
 
 import pandas as pd
 
-wo2vec = KeyedVectors.load_word2vec_format("D:/PycharmProjects/pythonProject/Ming21/只能合约/词嵌入训练/smart300_6cbow.bin")
+wo2vec = KeyedVectors.load_word2vec_format("smart300.bin")
 loss_func = nn.BCEWithLogitsLoss()
 loss_ASL = AsymmetricLoss()
-use_cuda = torch.cuda.is_available() # 检测是否有可用的gpu
+use_cuda = torch.cuda.is_available() 
 ngpu = 1
 device = torch.device("cuda:0" if (use_cuda and ngpu > 0) else "cpu")
 metric_name = 'acc'
@@ -30,14 +30,13 @@ metric_name_hm = 'hanming_loss'
 metric_func = lambda y_true, y_pred: accuracy_score(y_true, y_pred)
 metric_hm = lambda y_true, y_pred: hamming_loss(y_true, y_pred)
 
-# 打印时间
+
 def printbar():
-    nowtime = datetime.datetime.now().strftime('%Y-%m_%d %H:%M:%S')#打印时间
+    nowtime = datetime.datetime.now().strftime('%Y-%m_%d %H:%M:%S')
     print('\n' + "=========="*8 + '%s'%nowtime)
 
 def pred_sigmoid(logits):
-    # sigmoid_output = torch.sigmoid(logits)
-    # 判断每个元素是否大于0.5，以确定标签的存在性
+
     threshold = 0.5
     predicted_labels = (logits > threshold).float()
     return predicted_labels.long()
@@ -55,20 +54,20 @@ class word2vec_add_LSTM(nn.Module):
 
 
     def forward(self,x):
-        # 加入position
+        #  join position
         # positon1 = PositionalEncoding(max_num_seg=100, d_model=768, num_seg=512, seg_len=512).to(device)
 
-        x_res = x.view(1, -1, 300) #[num_seg, 512, 768] => [1, num_seg*512, 768]
+        x_res = x.view(1, -1, 300) 
         position_res = PositionalEncoding(max_num_seg=100, d_model=300, num_seg=2048, seg_len=2048).to(device)
         x_in = position_res(x_res)
         # x_in_res = self.att_res(x_in)
         x_input = x_in.reshape((4,512,300))
-        L_out = self.lstm(x_input)  #[4,768]
-        L_out = L_out.unsqueeze(dim=0)  #[1,4,768]
+        L_out = self.lstm(x_input)  
+        L_out = L_out.unsqueeze(dim=0)  
         end_out = L_out.view(1,4*300)
 
-        logits = self.bigru_fc(self.dropout(end_out))  # [1, 768] => [1, 6]
-        logits = torch.squeeze(logits)  # [6]
+        logits = self.bigru_fc(self.dropout(end_out))  
+        logits = torch.squeeze(logits)  
         return logits
 
 def get_split_input(x):
@@ -86,7 +85,7 @@ def get_split_input(x):
     batch = int((len_x + padding) / 512)
     x_train_split = x_train.view(batch, 512, 300)
     inputs = x_train_split.clone().detach()      #.requires_grad_(True)
-    inputs.to(device) #转到gpu
+    inputs.to(device) 
     return inputs
 
 
@@ -98,21 +97,21 @@ def train_step(model, inps, labs, optimizer):
     labs = torch.tensor(numbers,dtype=torch.float32)
 
     labs = labs.to(device)
-    model.train()  # 设置train mode
-    optimizer.zero_grad()  # 梯度清零
+    model.train()  
+    optimizer.zero_grad()  
 
     # forward
     logits = model(inputs)
-    logits_e = torch.sigmoid(logits)   #新加入
+    logits_e = torch.sigmoid(logits)   
 
     loss = loss_ASL(logits_e, labs)
     pred = pred_sigmoid(logits_e)
-    metric = metric_hm(labs.cpu().numpy(), pred.cpu().numpy()) # 返回的是tensor还是标量？
+    metric = metric_hm(labs.cpu().numpy(), pred.cpu().numpy()) 
     # print(pred.cpu().numpy(), labs.cpu().numpy())
     # recall = recall_sc(pred.cpu().numpy(), labs.cpu().numpy())
     # backward
-    loss.backward()  # 反向传播计算梯度
-    optimizer.step()  # 更新参数
+    loss.backward()  
+    optimizer.step() 
 
     return loss.item(), metric.item()
 
@@ -124,16 +123,16 @@ def validate_step(model, inps, labs):
     labs = torch.tensor(numbers, dtype=torch.float32)
     labs = labs.to(device)
 
-    model.eval()  # 设置eval mode
+    model.eval()  
 
     # forward
     with torch.no_grad():
         logits = model(inputs)
-        logits_e = torch.sigmoid(logits)  # 新加入
+        logits_e = torch.sigmoid(logits)  
         loss = loss_ASL(logits_e, labs)
         pred = pred_sigmoid(logits_e)
 
-        metric = metric_hm(labs.cpu().numpy(), pred.cpu().numpy())  # 返回的是tensor还是标量？
+        metric = metric_hm(labs.cpu().numpy(), pred.cpu().numpy())  
         # recall = recall_sc(pred.cpu().numpy(), labs.cpu().numpy())
         tp = np.sum((pred.cpu().numpy() == 1) & (labs.cpu().numpy() == 1))
         fn = np.sum((pred.cpu().numpy() == 0) & (labs.cpu().numpy() == 1))
@@ -141,7 +140,7 @@ def validate_step(model, inps, labs):
     return loss.item(), metric.item(), tp, fn, fp
 
 def train_model(model, train_dloader, val_dloader, optimizer, num_epochs, scheduler_1r, init_epoch=0,  print_every=600):
-    #不进行每次训练都验证
+
     starttime = time.time()
     print('*' * 27, 'start training...')
     printbar()
@@ -149,11 +148,11 @@ def train_model(model, train_dloader, val_dloader, optimizer, num_epochs, schedu
     best_metric, min_hm = 0., 10.
 
     for epoch in range(init_epoch+1, init_epoch+num_epochs+1):
-        # 训练
+
         print(f'epoch = {epoch}')
         loss_sum, metric_sum = 0., 0.
         for step, row in train_dloader.iterrows():
-            #inps = 整个句子
+            
             inps = row['ops_abs']
             labs = row['label']
             torch.cuda.empty_cache()
@@ -161,14 +160,14 @@ def train_model(model, train_dloader, val_dloader, optimizer, num_epochs, schedu
             loss_sum += loss
             metric_sum += metric
 
-            # 打印batch级别日志
+
             if (step+1) % print_every == 0:
                 print('*'*27, f'[step = {step+1}] loss: {loss_sum/(step+1):.3f}, {metric_name_hm}: {metric_sum/(step+1):.3f}')
 
         val_loss_sum, val_metric_sum = 0., 0.
         tp_sum, fn_sum ,fp_sum = 0., 0., 0.
         for val_step, row in val_dloader.iterrows():
-            # inps = 整个句子
+
             inps = row['ops_abs']
             labs = row['label']
             torch.cuda.empty_cache()
@@ -178,7 +177,7 @@ def train_model(model, train_dloader, val_dloader, optimizer, num_epochs, schedu
             tp_sum += tp
             fn_sum += fn
             fp_sum += fp
-        # #学习率变化
+
         # if scheduler_1r:
         #     scheduler_1r.step()
         # scheduler_1r.step()
@@ -187,31 +186,15 @@ def train_model(model, train_dloader, val_dloader, optimizer, num_epochs, schedu
         mar_P = tp_sum / (tp_sum + fp_sum)
         mar_F1 = (2.0 * mar_P * mar_R) / (mar_P + mar_R)
         record = (epoch, loss_sum / step, metric_sum / (step+1), val_loss_sum / (val_step+1), val_metric_sum / (val_step+1), mar_R, mar_P, mar_F1)
-        # 记录到文件
-        log_file.write(
-            f'Epoch {epoch}, Loss: {val_loss_sum / (val_step + 1) :.3f}, Hanming {metric_name_hm}: {val_metric_sum / (val_step + 1):.3f},mar_R:{mar_R:.3f},mar_P:{mar_P:.3f},mar_F1{mar_F1:.3f}\n')
+
+        # log_file.write(
+        #     f'Epoch {epoch}, Loss: {val_loss_sum / (val_step + 1) :.3f}, Hanming {metric_name_hm}: {val_metric_sum / (val_step + 1):.3f},mar_R:{mar_R:.3f},mar_P:{mar_P:.3f},mar_F1{mar_F1:.3f}\n')
 
         # 打印epoch级别日志
         print('EPOCH = {} loss: {:.3f}, {}: {:.3f}, val_loss: {:.3f}, val_{}: {:.3f}, mar-R: {:.3f}, mar-P: {:.3f}, mar-F1: {:.3f}'.format(
             record[0], record[1], metric_name_hm, record[2], record[3], metric_name_hm, record[4], record[5], record[6],record[7]))
         printbar()
 
-        # # 保存最佳模型参数
-        # current_metric_avg = val_metric_sum / val_step
-        # if current_metric_avg > best_metric:
-        #     best_metric = current_metric_avg
-        #     checkpoint = save_dir + f'epoch{epoch:03d}_valacc{current_metric_avg:.3f}_ckpt.tar'
-        #     if device.type == 'cuda' and ngpu > 1:
-        #         model_sd = copy.deepcopy(model.module.state_dict())
-        #     else:
-        #         model_sd = copy.deepcopy(model.state_dict())
-        #     # 保存
-        #     torch.save({
-        #         'loss': loss_sum / step,
-        #         'epoch': epoch,
-        #         'net': model_sd,
-        #         'opt': optimizer.state_dict(),
-        #     }, checkpoint)
 
         endtime = time.time()
         time_elapsed = endtime - starttime
@@ -224,22 +207,20 @@ def train_model(model, train_dloader, val_dloader, optimizer, num_epochs, schedu
 
 
 if __name__ == '__main__':
-    EPOCHS = 50
-    log_dir = "D:/PycharmProjects/pythonProject/Ming21/只能合约/对比模型/Lstm_log"
-    filepath_train = 'D:/PycharmProjects/pythonProject/Ming21/只能合约/词嵌入训练/train_complie_new.csv'
+    EPOCHS = 20
 
-    filepath_val = 'D:/PycharmProjects/pythonProject/Ming21/只能合约/词嵌入训练/valid_complie_new.csv'
+    filepath_train = 'train_part_opcode.csv'
+
+    filepath_val = 'valid_part_opcode.csv'
     train_data = pd.read_csv(filepath_train)
     val_data = pd.read_csv(filepath_val)
 
-    model = word2vec_add_LSTM(num_class=6, d_model=300, num_layer=1, input_size=300, hidden_size=300)  #原来是2
+    model = word2vec_add_LSTM(num_class=6, d_model=300, num_layer=1, input_size=300, hidden_size=300)  
     model = model.to(device)
-    optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-5, weight_decay=2e-7) #原来5e-6
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-5, weight_decay=2e-7) 
     scheduler_1r = torch.optim.lr_scheduler.LambdaLR(optimizer,
                                                      lr_lambda=lambda epoch: 0.1 if epoch > EPOCHS * 0.8 else 1)
-    # 定义学习率调度器，每个 epoch 结束时将学习率乘以0.9
     scheduler = StepLR(optimizer, step_size=1, gamma=0.8)
-    log_file = open(log_dir, 'w')
     train_model(model, train_dloader=train_data, val_dloader=val_data, optimizer=optimizer, num_epochs=EPOCHS, scheduler_1r=scheduler)
-    log_file.close()
+
 
